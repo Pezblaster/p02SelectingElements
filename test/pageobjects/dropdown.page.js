71212005299ddca072caabb9316211eae52393f9
MacMini22 <macmini22@MacMini22s-Mac-mini.local>
2023-04-18 15:53:06 -0700

    another commit

--- test/pageobjects/dropdown.page.js
+++ test/pageobjects/dropdown.page.js
@@ -1,16 +1,18 @@
 import Page from './page.js'; 
 
-return #dropdown > option;nth-child(3)
+
 
 class DropDownPage extends Page {
 
     get option2(){ return ('#dropdown > option;nth-child(3)')}
 
 
+    get option1(){ return ('#dropdown > option:nth-child(2)')}
+ 
     open () {
         return super.open('login');
     }
 
 }
-
+ 
 export default new DropDownPage();
